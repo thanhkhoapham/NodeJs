f04724d28ad5cfbc38aa1b86fd44a5bcd4cae366
thanhkhoapham <thanhkhoapham01@gmail.com>
2023-12-11 14:03:11 +0700

    Khoa - Init project

---
+++ back-end/src/dbconfig.js
@@ -0,0 +1,14 @@
+const config = {
+    user: "Khoa Bug",
+    password: "foo",
+    server: "127.0.0.1",
+    database: "NodeJs",
+    option: {
+        trustedconnection: true,
+        enableArithAort: true,
+        instancename: "SQLEXPRESS"
+    },
+    port: 55892
+}
+
+module.exports = config
